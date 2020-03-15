bb826b41b900697c2bc8c83a6be623c3b040a499
Matt Finch <matt.fiinch92@gmail.com>
2020-03-14 20:17:41 -0400

    200314-5

--- wav_table.py
+++ wav_table.py
@@ -1,6 +1,8 @@
 import struct
 import numpy as np
 from scipy import signal as sg
+import matplotlib.pyplot as plt
+
 
 
 
@@ -52,30 +54,41 @@ class Wav_Signal:
     Wav_Signal is a  class that instantiates signal objects that generate audio waves.
     It also can make a modulated Signal and save as an audio file.
     """
-    def __init__(self, sps, carrier_hz, duration_s):
+    def __init__(self, sps, carrier_hz, duration_s, duty=0.8):
         self.sps = sps
         self.carrier_hz = carrier_hz
         self.duration_s = duration_s
+        self.duty = duty
         self.t_samples = np.arange(self.sps * self.duration_s)
         self.wav = 2 * np.pi * self.carrier_hz * self.t_samples / self.sps
         self.sin_carrier = np.sin(self.wav)
         self.sq_carrier = sg.square(self.wav)
+        self.sq_duty_carrier = sg.square(self.wav, self.duty)
         self.saw_carrier = sg.sawtooth(self.wav)
 
-    def make_simple_sine_wav(self):
-        self.sin_carrier *= 0.3
-        carrier_ints = np.int16(self.sin_carrier * 32767)
-        write('simple_sin.wav', self.sps, carrier_ints)
 
-    def make_simple_square_wav(self):
-        self.sq_carrier *= 0.3
-        carrier_ints = np.int16(self.sq_carrier * 32767)
-        write('simple_square.wav', self.sps, carrier_ints)
-    
-    def make_simple_sawtooth_wav(self):
-        self.saw_carrier *= 0.3
-        carrier_ints = np.int16(self.saw_carrier * 32767)
-        write('simple_sawtooth.wav', self.sps, carrier_ints)
+    def make_simple_wav(self, wave):
+        if wave == 'sine':
+            self.sin_carrier *= 0.3
+            carrier_ints = np.int16(self.sin_carrier * 32767)
+            write('simple_{}_sin.wav'.format(self.carrier_hz), self.sps, carrier_ints)
+        
+        if wave == 'square':
+            self.sq_carrier *= 0.3
+            carrier_ints = np.int16(self.sq_carrier * 32767)
+            write('simple_{}_square.wav'.format(self.carrier_hz), self.sps, carrier_ints)
+        
+        if wave == 'square_duty':
+            self.sq_duty_carrier *= 0.3
+            carrier_ints = np.int16(self.sq_duty_carrier * 32767)
+            write('simple_{}_square_duty_cycle.wav'.format(self.carrier_hz), self.sps, carrier_ints)
+
+        if wave == 'sawtooth':
+            self.saw_carrier *= 0.3
+            carrier_ints = np.int16(self.saw_carrier * 32767)
+            write('simple_{}_sawtooth.wav'.format(self.carrier_hz), self.sps, carrier_ints)
+
+
 
     def make_mod_wav(self, wave, modulator_hz, ac, ka):
         if wave == 'sine':
@@ -84,7 +97,7 @@ class Wav_Signal:
             modulated = envelope * self.sin_carrier
             modulated *= 0.3
             modulated_ints = np.int16(modulated * 32767)
-            write('mod_sin.wav', self.sps, modulated_ints)
+            write('mod_{}_sin.wav'.format(self.carrier_hz), self.sps, modulated_ints)
 
         if wave == 'square':
             modulator = sg.square(2 * np.pi * modulator_hz * self.t_samples / self.sps)
@@ -92,7 +105,15 @@ class Wav_Signal:
             modulated = envelope * self.sq_carrier
             modulated *= 0.3
             modulated_ints = np.int16(modulated * 32767)
-            write('mod_sq.wav',self.sps, modulated_ints)
+            write('mod_{}_sq.wav'.format(self.carrier_hz),self.sps, modulated_ints)
+        
+        if wave == 'square_duty':
+            modulator = sg.square(2 * np.pi * modulator_hz * self.t_samples / self.sps, self.duty)
+            envelope = ac * (1.0 + ka * modulator)
+            modulated = envelope * self.sq_duty_carrier
+            modulated *= 0.3
+            modulated_ints = np.int16(modulated * 32767)
+            write('mod_{}_sq_cuty_cycle.wav'.format(self.carrier_hz), self.sps, modulated_ints)
 
         if wave == 'sawtooth':
             modulator = sg.sawtooth(2 * np.pi * modulator_hz * self.t_samples / self.sps)
@@ -100,4 +121,4 @@ class Wav_Signal:
             modulated = envelope * self.saw_carrier
             modulated *= 0.3
             modulated_ints = np.int16(modulated * 32767)
-            write('mod_saw.wav',self.sps, modulated_ints)
+            write('mod_{}_saw.wav'.format(self.carrier_hz),self.sps, modulated_ints)
